b469002c49cb0d8a16a772b92820ba369f5365ee
Cyril <devcyril83@gmail.com>
2024-07-16 12:33:57 +0200

    ajout du formulaire d'ajout de collection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from '../component/not-found/not-found.component';
 import { HomeComponent } from '../component/home/home.component';
 import { CollectionsComponent } from '../component/Collection/collections/collections.component';
+import { AddCollectionComponent } from '../component/Collection/add-collection/add-collection.component';
 
 export const routes: Routes = [
     {path : "",component:HomeComponent},
     {path : "collections",component:CollectionsComponent},
+    {path : "add-collection",component:AddCollectionComponent},
     {path : "**",component:NotFoundComponent}
 ];
--- src/component/Collection/add-collection/add-collection.component.ts
+++ src/component/Collection/add-collection/add-collection.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from '../../../services/api.service';
+import { Collection } from '../../../utils/interface/Collection';
 
 @Component({
   selector: 'app-add-collection',
@@ -8,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrl: './add-collection.component.css'
 })
 export class AddCollectionComponent {
+
+  addCollectionGroup = new FormGroup({
+    name : new FormControl<string>(""),
+    description : new FormControl<string>("")
+  })
+
+  constructor(private api : ApiService){}
+  collection : Collection={
+    id: 0,
+    name: "",
+    description: "",
+    ImageId: 0
+  }
+
+  ngOnInit(){
+
+  }
+
+  onSubmit(){
+    if(this.collection){
+      if(!this.addCollectionGroup.value.name)return;
+      if(!this.addCollectionGroup.value.description)return;
+
+      this.collection.name = this.addCollectionGroup.value.name;
+      this.collection.description = this.addCollectionGroup.value.description;
+
+      this.api.postCollection(this.collection)
+      .then(data=>{console.log(data)})
+    }
+  }
+
 
 }
