35a0fa0887da0ab9ff6055e0232a00d732b2c7e5
Cyril <devcyril83@gmail.com>
2024-07-17 14:42:25 +0200

    ajout de la fonctionalité delete pour les collections

--- src/component/Collection/add-collection/add-collection.component.ts
+++ src/component/Collection/add-collection/add-collection.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../../services/api.service';
 import { Collection } from '../../../utils/interface/Collection';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-collection',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './add-collection.component.html',
   styleUrl: './add-collection.component.css'
 })
 export class AddCollectionComponent {
+  [x: string]: any;
 
   addCollectionGroup = new FormGroup({
     name : new FormControl<string>(""),
     description : new FormControl<string>("")
   })
 
-  constructor(private api : ApiService){}
+  constructor(private api : ApiService, private router : Router){}
   collection : Collection={
     id: 0,
     name: "",
@@ -30,15 +32,19 @@ export class AddCollectionComponent {
   }
 
   onSubmit(){
+    console.log("hellooo")
     if(this.collection){
       if(!this.addCollectionGroup.value.name)return;
       if(!this.addCollectionGroup.value.description)return;
 
       this.collection.name = this.addCollectionGroup.value.name;
       this.collection.description = this.addCollectionGroup.value.description;
-
+      console.log(this.collection.name)
+      console.log(this.collection)
       this.api.postCollection(this.collection)
       .then(data=>{console.log(data)})
+
+      this.router.navigateByUrl("/collections")
     }
   }
 
