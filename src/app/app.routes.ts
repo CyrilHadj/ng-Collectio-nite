023ea51ee018507b3b00591f279d9924f20adfd3
Cyril <devcyril83@gmail.com>
2024-07-18 13:07:00 +0200

    création des component : items , add-item , delete-item , update-item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from '../component/home/home.component';
 import { CollectionsComponent } from '../component/Collection/collections/collections.component';
 import { AddCollectionComponent } from '../component/Collection/add-collection/add-collection.component';
 import { UpdateCollectionComponent } from '../component/Collection/update-collection/update-collection.component';
+import { ItemsComponent } from '../component/Item/items/items.component';
 
 export const routes: Routes = [
     {path : "",component:HomeComponent},
     {path : "collections",component:CollectionsComponent},
     {path : "add-collection",component:AddCollectionComponent},
     {path : "update-collection/:collectionId",component:UpdateCollectionComponent},
+    {path : "items/:collectionId",component:ItemsComponent},
     {path : "**",component:NotFoundComponent}
 ];
